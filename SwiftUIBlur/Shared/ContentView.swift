df0370551d84e900ff0d65b64404368a61521a5e
Egor Merkushev <egor.merkushev@corp.vk.com>
2020-11-22 01:23:02 +0300

    Added template SwiftUI project

---
+++ SwiftUIBlur/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Egor Merkushev on 22.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBlur/Shared/SwiftUIBlurApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBlurApp.swift
+//  Shared
+//
+//  Created by Egor Merkushev on 22.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBlurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
